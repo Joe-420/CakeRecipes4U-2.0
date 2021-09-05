d91013d94079286b5846f84f64813e4566c19047
cakeboss <61979750+Joe-420@users.noreply.github.com>
2021-09-05 14:35:58 -0300

    Update PlayCakeQuiz.js

--- PlayCakeQuiz.js
+++ PlayCakeQuiz.js
@@ -107,7 +107,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/EndCakeQuiz.htm')
+        return window.location.assign('EndCakeQuiz.htm')
     }
 
     questionCounter++
